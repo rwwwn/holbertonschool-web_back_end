16ec24da3abae3a901f58fdfd56b00114f588b24
rwwwn <ralbaraiki@gmail.com>
2025-07-28 18:39:10 +0300

    Update 8-make_multiplier.py

--- python_variable_annotations/8-make_multiplier.py
+++ python_variable_annotations/8-make_multiplier.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
+"""This module defines a function that creates a multiplier function."""
+
 from typing import Callable
 
+
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """Return a function that multiplies a float by the given multiplier."""
     return lambda x: x * multiplier
