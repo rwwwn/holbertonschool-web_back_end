764b72aaa3fac5c68846a451895bde4c8cf1b77f
rwwwn <ralbaraiki@gmail.com>
2025-07-28 18:38:14 +0300

    Update 7-to_kv.py

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
+"""This module defines a function that returns a key-value tuple."""
+
 from typing import Union, Tuple
 
+
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """Return a tuple with a string and the square of an int or float."""
     return (k, float(v ** 2))
