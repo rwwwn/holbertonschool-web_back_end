d110818528e4b489de4be1a2df73ce5e4e5247b7
rwwwn <ralbaraiki@gmail.com>
2025-07-28 18:30:30 +0300

    Update 1-concat.py

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,3 +1,8 @@
 #!/usr/bin/env python3
+"""This module provides a function to concatenate two strings."""
+
+
 def concat(str1: str, str2: str) -> str:
+    """Return the concatenation of two strings."""
     return str1 + str2
+
