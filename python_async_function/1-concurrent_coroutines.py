24f0542d398b67bf32faa4d52a7b9c75b156d4b3
rwwwn <ralbaraiki@gmail.com>
2025-07-28 18:52:05 +0300

    Update 1-concurrent_coroutines.py

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """
-This module defines a coroutine that runs multiple asynchronous tasks concurrently
-and returns a list of their completion times in ascending order.
+This module provides an async function that runs multiple coroutines concurrently.
 """
 
 import asyncio
@@ -11,8 +10,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """
-    Spawns wait_random n times with the specified max_delay.
-    Returns a list of the delays in ascending order.
+    Run wait_random n times concurrently and return the list of delays in ascending order.
     """
     delays = await asyncio.gather(*(wait_random(max_delay) for _ in range(n)))
     return sorted(delays)
