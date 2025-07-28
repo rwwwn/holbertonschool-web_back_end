a78f304cbb4c5eceeb41f36ced6013dffa478793
rwwwn <ralbaraiki@gmail.com>
2025-07-28 20:31:37 +0300

    Update 2-main.py

--- python_async_comprehension/2-main.py
+++ python_async_comprehension/2-main.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
 import asyncio
+
 measure_runtime = __import__('2-measure_runtime').measure_runtime
 
-async def main():
-    print(await measure_runtime())
 
-asyncio.run(main())
+async def main():
+    return await measure_runtime()
+
+print(asyncio.run(main()))
