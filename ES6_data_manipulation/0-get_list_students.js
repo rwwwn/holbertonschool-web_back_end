345c35c631c82fb2a5caed966cf3ed8304b3a3b6
Rawan <ralbaraiki@gmail.com>
2025-09-04 18:35:15 +0300

    Delete ES6_data_manipulation directory

--- ES6_data_manipulation/0-get_list_students.js
+++
@@ -1,9 +0,0 @@
-// Return the required list of student objects
-export default function getListStudents() {
-  return [
-    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' },
-  ];
-}
-
